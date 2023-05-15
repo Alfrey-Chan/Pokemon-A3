009c0cfad0b65459b0540cd70d5707631ba5743e
Alfrey Chan <alfreychan@gmail.com>
2023-05-15 08:49:43 -0700

    Implement pagination to divide pokemons per page

--- pokemon.js
+++ pokemon.js
@@ -1,5 +1,18 @@
+var pokemon = [];
+const pokemonPerPage = 10;
+var numPages = 0;
+const numPageBtn = 5;
+
+
 const setup = async () => {
   const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810");
+  console.log(response.data.results);
+
+  pokemon = response.data.results;
+  numPages = Math.ceil(pokemon.length / pokemonPerPage);
+  console.log("numPages: ", numPages);
+
+  showPage(1);
 
   $('body').on('click', '.pokemonCard', async function (e) {
     console.log(this);
@@ -32,11 +45,31 @@ const setup = async () => {
 
 
 
-  const pokemon = response.data.results;
+  $('body').on('click', '.pageBtn', async function (e) {
+    const pageNum = parseInt($(this).attr('pageNum'));
+    console.log("pageBtn clicked");
+    console.log("pageNum: ", pageNum);
+    showPage(pageNum);
+  });
+
+
+};
+
+async function showPage(currentPage) {
+  if (currentPage < 1) {
+    currentPage = 1;
+  }
+  if (currentPage > numPages) {
+    currentPage = numPages;
+  }
+  console.log("currentPage: ", currentPage);
+
   $('#pokemon').empty();
-  for (let pokemonIndex = 0; pokemonIndex < pokemon.length; pokemonIndex++) {
+  // for (let pokemonIndex = 0; pokemonIndex < pokemon.length; pokemonIndex++) {
+    for (let pokemonIndex = ((currentPage - 1) * pokemonPerPage); pokemonIndex < ((currentPage - 1) * pokemonPerPage) + pokemonPerPage && pokemonIndex < pokemon.length; pokemonIndex++) {
     let innerResponse = await axios.get(`${pokemon[pokemonIndex].url}`);
     let pokemonInfo = innerResponse.data;
+    console.log("test");
     $('#pokemon').append(`
       <div class="pokemonCard" pokeName=${pokemonInfo.name}>
         <h3>${pokemonInfo.name}</h3>
@@ -46,7 +79,33 @@ const setup = async () => {
       `)
   }
 
-};
+  $('#pagination').empty();
+  var startPage = Math.max(1, currentPage - Math.floor(numPageBtn / 2));
+  var endPage = Math.min(numPages, currentPage + Math.floor(numPageBtn / 2));
+  console.log("startPage: ", startPage);
+  console.log("endPage: ", endPage);
+
+  if (currentPage > 1) {
+    $('#pagination').append(`
+      <button type="button" class="btn btn-primary pageBtn" id="page1" pageNum="1">First</button>
+      <button type="button" class="btn btn-primary pageBtn" id="page${currentPage - 1}" pageNum="${currentPage - 1}">Previous</button>
+      `)
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    $('#pagination').append(`
+      <button type="button" class="btn btn-primary pageBtn" id="page${i}" pageNum="${i}">${i}</button>
+      `)
+  }
+
+  if (currentPage < numPages) {
+    $('#pagination').append(`
+      <button type="button" class="btn btn-primary pageBtn" id="page${currentPage + 1}" pageNum="${currentPage + 1}">Next</button>
+      <button type="button" class="btn btn-primary pageBtn" id="page${numPages}" pageNum="${numPages}">Last</button>
+      `)
+  }
+}
+
 
 
 $(document).ready(setup);
