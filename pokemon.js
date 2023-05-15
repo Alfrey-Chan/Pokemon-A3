cb03f500cd5ba5377e8362ec48f98e33cb30663b
Alfrey Chan <alfreychan@gmail.com>
2023-05-15 00:07:38 -0700

    Display pokemon cards dynamically

--- pokemon.js
+++ pokemon.js
@@ -1,6 +1,20 @@
 const setup = async () => {
   const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=810");
-  console.log(response.data.results);
+
+  const pokemon = response.data.results;
+  $('#pokemon').empty();
+  for (let pokemonIndex = 0; pokemonIndex < pokemon.length; pokemonIndex++) {
+    let innerResponse = await axios.get(`${pokemon[pokemonIndex].url}`);
+    let pokemonInfo = innerResponse.data;
+    $('#pokemon').append(`
+      <div class="pokemonCard card">
+        <h3>${pokemonInfo.name}</h3>
+        <img src="${pokemonInfo.sprites.front_default}" alt="${pokemonInfo.name}">
+        <button type="button" class="btn btn-primary">More Info</button>
+      </div>
+      `)
+  }
+
 };
 
 
